e113598462c6787c33d8995c6337274bff14e88d
Ravi <rchellaboina@gmail.com>
2020-11-03 14:50:37 +0530

    dDDED ANOTHER COMMENT
    Updated the file for new Branch

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 def main(msg):
-    # type: (object) -> object
-    print(msg)
-    # Adding a second comment to this line to check conflict
 main("Hello Ubuntu People")
-    # Adding Comment Line
-    #Adding Another Comment
-main("Hello Ubuntu People")
+#No Longer need comments here
+def secondary():
+    Print("New Function")
